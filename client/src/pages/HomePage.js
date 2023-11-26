2e7f11d0f3e0d0a87cdb407f184ec69b2998538b
Mukul <mukultotla4@gmail.com>
2023-11-26 22:42:30 +0530

    quering jobs from client

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -1,12 +1,15 @@
-import JobList from '../components/JobList';
-import { jobs } from '../lib/fake-data';
+import JobList from "../components/JobList";
+import { getJobs } from "../lib/graphql/queries";
+import { useState, useEffect } from "react";
 
 function HomePage() {
+  const [jobs, setJobs] = useState([]);
+  useEffect(() => {
+    getJobs().then((jobs) => setJobs(jobs));
+  }, []);
   return (
     <div>
-      <h1 className="title">
-        Job Board
-      </h1>
+      <h1 className="title">Job Board</h1>
       <JobList jobs={jobs} />
     </div>
   );
