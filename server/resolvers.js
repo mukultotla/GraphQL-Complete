823b2eb00d9ec5b02eca5f2eac31fed7e8ff6350
Mukul <mukultotla4@gmail.com>
2023-11-26 22:42:52 +0530

    created job resolver

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,11 +1,9 @@
-import { getJobs } from "./db/jobs.js";
+import { getJobs, getJob } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
 export const resolvers = {
   Query: {
-    jobs: async () => {
-      const jobs = await getJobs();
-      return jobs;
-    },
+    job: (_, args) => getJob(args.id),
+    jobs: async () => getJobs(),
   },
   Job: {
     date: (job) => {
