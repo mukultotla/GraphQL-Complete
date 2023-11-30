3a27179cc53c963ec831ad03ee5e536d53e5ea69
Mukul <mukultotla4@gmail.com>
2023-11-30 22:57:38 +0530

    custom graphQL error

--- server/resolvers.js
+++ server/resolvers.js
@@ -1,16 +1,30 @@
 import { getJobs, getJob, getJobsByCompany } from "./db/jobs.js";
 import { getCompany } from "./db/companies.js";
+import { GraphQLError } from "graphql";
+
 export const resolvers = {
   Query: {
-    company: (_, args) => getCompany(args.id),
-    job: (_, args) => getJob(args.id),
+    company: async (_, args) => {
+      const company = await getCompany(args.id);
+      if (!company) {
+        notFoundError(`No company found with id ${args.id}`);
+      }
+      return company;
+    },
+    job: async (_, args) => {
+      const job = await getJob(args.id);
+      if (!job) {
+        notFoundError(`No job found with id ${args.id}`);
+      }
+      return job;
+    },
     jobs: async () => getJobs(),
   },
   Company: {
     jobs: (company) => {
       return getJobsByCompany(company.id);
-    }
-  }, 
+    },
+  },
   Job: {
     date: (job) => {
       return job.createdAt.slice(0, "yyyy-mm-dd".length);
@@ -20,3 +34,9 @@ export const resolvers = {
     },
   },
 };
+
+function notFoundError(message) {
+  throw new GraphQLError(message, {
+    extensions: { code: "NOT_FOUND" },
+  });
+}
